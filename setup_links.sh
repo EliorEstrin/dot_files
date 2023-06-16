bc6493adf8c23c918dd03f7bc892b0533c64a899
elior <elior1001@gmail.com>
2023-06-16 20:04:44 +0300

    allacrity and neovim

--- setup_links.sh
+++ setup_links.sh
@@ -1,4 +1,5 @@
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc 
 ln -s ~/dotfiles/p10k/p10k.zsh  ~/.p10k.zsh 
-
+ln -s ~/dotfiles/nvim/ ~/.config/nvim 
+ln -s ~/dotfiles/alacritty/alacritty.yml ~/.alacritty.yml
 
