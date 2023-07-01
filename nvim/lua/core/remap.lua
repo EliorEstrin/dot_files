edeb971ef0ce1b4dcceef93d19cbfd896cff9e87
elior <elior1001@gmail.com>
2023-07-02 00:07:12 +0300

    added key map of the legendery primegen

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -1,16 +1,22 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Nvim tree
 vim.keymap.set('n', '<C-t>', "<cmd>NvimTreeToggle<cr>", { silent = true })
 
+-- Disable space Bar deafult funcionality
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- keymaps to comment
-   vim.keymap.set('n', '<leader>/', function ()
-     require("Comment.api").toggle.linewise.current()
-   end)
--- Commanding in visual mode
-  vim.keymap.set('v', '<leader>/', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
+-- keymap to comment lines of code
+vim.keymap.set('n', '<leader>/', function ()
+ require("Comment.api").toggle.linewise.current()
+end)
+
+-- comment line in visual mode
+vim.keymap.set('v', '<leader>/', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
+
+-- greatest remap ever - All Credits to Primegen :) 
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
