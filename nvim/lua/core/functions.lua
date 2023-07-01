f99d3b32d75f4c13c29d42e4a3ff7abc45f813c0
elior <elior1001@gmail.com>
2023-07-01 22:54:16 +0300

    updated function in lua, and path added in zshrc

--- nvim/lua/core/functions.lua
+++ nvim/lua/core/functions.lua
@@ -12,7 +12,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Git Hub
 vim.api.nvim_create_user_command("Lazyg", function ()
   local commit_message = vim.fn.input("Commit message: ")
-  local command = string.format("git commit -am %q && git push origin dev", commit_message)
+  local current_git_branch = vim.fn.system("git rev-parse --abbrev-ref HEAD")
+  local command = string.format("git commit -am %q && git push origin %s", commit_message, current_git_branch)
   local output = vim.fn.system(command)
   print(output)
 end, {})
