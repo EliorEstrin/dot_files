96db2eb1774de99bc7ddabc7b0e4cea0ce5988fb
elior <elior.estrin@develeap.com>
2023-07-05 14:17:08 +0300

    refactor nvim added key remaps

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -6,6 +6,7 @@ vim.keymap.set('n', '<C-t>', vim.cmd.NvimTreeToggle, { silent = true })
 
 -- Disable space Bar deafult funcionality
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set("n", "Q", "<nop>")
 
 -- keymap to comment lines of code
 vim.keymap.set('n', '<leader>/', function ()
@@ -15,12 +16,27 @@ end)
 -- comment line in visual mode
 vim.keymap.set('v', '<leader>/', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
 
--- greatest remap ever - All Credits to Primegen :) 
+-- Moving Lines in Visual Mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Primegen Thank you for Sharing:
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
+-- This is Amazing
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Chmod easy
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Delete to void register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
