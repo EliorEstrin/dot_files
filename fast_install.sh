28f981b87dd1fefac48ad21df6bfafbea14d0ee3
elior <elior.estrin@develeap.com>
2023-07-08 22:04:45 +0300

    small changes in fast install script

--- fast_install.sh
+++ fast_install.sh
@@ -9,7 +9,6 @@ sudo apt install fzf -y
 sudo add-apt-repository ppa:aslatter/ppa -y 
 sudo apt install alacritty -y 
 sudo apt install zsh -y 
-sudo apt install git -y
 
 # OhMyZsh and Powerlevel10k Setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -20,21 +19,21 @@ sudo apt install build-essential -y
 sudo apt install clang -y
 
 # Zsh Custom Plugins
-cd 
+pushd
 git clone https://github.com/wting/autojump.git
 cd autojump
 python3 ./install.py
 # Fix autojump bug
 sed -i "s/\#\!\/usr\/bin\/env\ python$/\#\!\/usr\/bin\/env\ python3/" ~/.autojump/bin/autojump
+popd
 
 # OhMyTmux Installation
-cd
+pushd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
+popd
 
 # Dotfile Configuration
-git clone https://github.com/PhantomPixelist/dot_files.git
-cd dot_files
 cp -r zsh/scripts/zsh-autosuggestions/ ~/.oh-my-zsh/custom/plugins/ && cp -r zsh/scripts/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
 bash install
